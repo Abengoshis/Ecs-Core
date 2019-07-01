99312759196934b1e1c205bd7e6a46427aa098a7
Alex Saye <alexandersaye@live.co.uk>
2019-07-01 17:00:43 +0100

    Add a copy constructor and prevent LocalPose and GlobalPose from being serialized.

--- EcsCore/Components/Transform.cs
+++ EcsCore/Components/Transform.cs
@@ -1,26 +1,38 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace EcsCore.Components
 {
     public class Transform : IEcsComponent
     {
-        public Matrix LocalPose;
-        public Matrix GlobalPose;
-
         public Vector2 Position;
         public float Rotation;
 
-        public Transform(Vector2 position, float rotation = 0f)
-        {
-            LocalPose = Matrix.Identity;
-            GlobalPose = Matrix.Identity;
+        [ContentSerializerIgnore]
+        public Matrix LocalPose = Matrix.Identity;
 
-            Position = position;
+        [ContentSerializerIgnore]
+        public Matrix GlobalPose = Matrix.Identity;
+
+        public Transform(float x, float y, float rotation = 0f)
+        {
+            Position.X = x;
+            Position.Y = y;
             Rotation = rotation;
         }
 
-        public Transform(float x, float y, float rotation = 0f) : this(new Vector2(x, y), rotation)
+        public Transform(Vector2 position, float rotation = 0f) : this(position.X, position.Y, rotation)
         {
+        }
+
+        public Transform() : this(0, 0, 0)
+        {
+        }
+
+        public Transform(Transform transform) : this(transform.Position, transform.Rotation)
+        {
+            LocalPose = transform.LocalPose;
+            GlobalPose = transform.GlobalPose;
         }
     }
 }
